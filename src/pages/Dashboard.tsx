fda2e88cb35f3910dc46edce515366088163a8e6
Cristopher Yates <cmoyates@gmail.com>
2021-09-24 20:30:44 -0230

    Fixed dependancy issue and started blocking out dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,23 @@
+import { AppBar, Grid, TextField, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 const Dashboard = () => {
+
+    const a = [1,1,1,1,1,1,1];
+
     return (
         <div>
-            Dashboard
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h5">Dashboard</Typography>
+                </Toolbar>
+            </AppBar>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <Grid container>
+                {a.map((item, index)=><Grid item xs={6} key={index}>
+                    Test
+                </Grid>)}
+            </Grid>
         </div>
     )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Button } from '@mui/material';
+import React from 'react';
+import { useHistory } from 'react-router';
 
 const LandingPage = () => {
+    const history = useHistory();
+
     return (
         <div>
             <h1>Landing Page</h1>
+            <Button variant="contained" onClick={()=>{
+                history.push("/dashboard");
+            }}>
+                Dashboard
+            </Button>
         </div>
     )
 }
