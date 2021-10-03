47265223250196d386c1a89d734d8f6e0e1c105a
Cristopher Yates <cmoyates@gmail.com>
2021-10-03 03:42:57 -0230

    Styled the Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
-import { Button, Container, Stack } from '@mui/material';
+import { Button, Container, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useHistory } from 'react-router';
+import GoogleIcon from '@mui/icons-material/Google';
 
 interface Props {
 
@@ -15,14 +16,14 @@ const LandingPage: React.FC<Props> = (props) => {
     }
 
     return (
-        <div>
-            <h1>Landing Page</h1>
-            <Container maxWidth="sm">
-                <Stack direction="column" alignItems="center" spacing={2}>
-                    <Button variant="contained" onClick={()=>{handleLogin();}}>
+        <div className="bgImage">
+            <Container>
+                <Stack height="100vh" alignItems="start" justifyContent="center">
+                    <Typography variant="h1" padding={2} color="white"><b>Advice Bank</b></Typography>
+                    <Button size="large" variant="outlined" style={{color: "white", borderColor: "white", marginLeft: "16px"}} onClick={()=>{handleLogin();}} startIcon={<GoogleIcon />}>
                         Login with Google
                     </Button>
-                    <Button variant="contained" onClick={()=>{history.push("/dashboard");}}>
+                    <Button size="medium" style={{color: "white", marginLeft: "10px"}} onClick={()=>{history.push("/dashboard");}}>
                         Continue without logging in
                     </Button>
                 </Stack>
